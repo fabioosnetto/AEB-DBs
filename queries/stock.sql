b4cb72f5195b9e537a53e200745a277043a1b983
fabioosnetto <fabioorione8@gmail.com>
2023-01-04 08:57:04 -0300

    part and relatory tables
    --> Table for the stock parts created;
    --> Table for the relatory of parts created;
    
    *Queries for creating all the stock structure basically finisshed.
    **No data added (begins in a new script).

--- queries/stock.sql
+++ queries/stock.sql
@@ -1,3 +1,7 @@
+/*+------------------------------------------+*/
+/*|      QUERIES FOR CREATING THE STOCK      |*/
+/*+------------------------------------------+*/
+
 /*------ CREATE DATABASE FOR STOCK ------*/
 DROP DATABASE IF EXISTS stock;
 CREATE DATABASE IF NOT EXISTS stock;
@@ -21,4 +25,14 @@ CREATE TABLE IF NOT EXISTS part (
    description tinytext,
    PRIMARY KEY (pCode),
    FOREIGN KEY (location) REFERENCES structure (id)
+)engine = InnoDB default charset = utf8;
+
+/*------ CREATE TABLE FOR THE PARTS RELATORY ------*/
+CREATE TABLE IF NOT EXISTS relatory(
+	id int not null auto_increment,
+   pCode int not null,
+   situation enum('NOTED', 'NOT NOTED') not null default 'NOT NOTED',
+   delivery date,
+   PRIMARY KEY(id),
+   FOREIGN KEY(pCode) REFERENCES part(pCode)
 )engine = InnoDB default charset = utf8;
